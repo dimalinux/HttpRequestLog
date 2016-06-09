8972ff5c06556baefc10d431a484dcbc9b458ce2
Дмитрий Холодов <github@sec.to>
2016-06-08 23:50:48 -0500

    mapped our single-page app locations (/#/**) to the homepage even though fragments shouldn't normally be sent to the server

--- src/main/java/to/xss/httprequestlog/controller/UserViewController.java
+++ src/main/java/to/xss/httprequestlog/controller/UserViewController.java
@@ -6,18 +6,20 @@ package to.xss.httprequestlog.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.HandlerMapping;
 import to.xss.httprequestlog.domain.RequestEntity;
 import to.xss.httprequestlog.domain.RequestRepository;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 import java.util.Map;
@@ -30,7 +32,13 @@ public class UserViewController {
     @Autowired
     RequestRepository requestRepository;
 
-    @RequestMapping(value = {"/"}, method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
+    // Some brain-dead bots and people manipulating request headers would send the location fragment to the server,
+    // so I mapped our single-page app locations (/#/**) to the homepage so the requests are not logged publicly.
+    @RequestMapping(
+            value = {"/", "/#/**"},
+            method = RequestMethod.GET,
+            produces = MediaType.TEXT_HTML_VALUE
+    )
     public String showLandingPage(HttpServletResponse response) {
         log.info("homepage request made");
         response.addHeader("X-Frame-Options", "deny");
@@ -41,7 +49,9 @@ public class UserViewController {
         return "_view/index.html";
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.POST,
+    @RequestMapping(
+            value = "/",
+            method = RequestMethod.POST,
             produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
             consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
